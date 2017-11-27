82ec720fa4298d56c11d99e3dca54d5f9a1a457b
eviltik <eviltik@gmail.com>
2017-11-27 12:55:53 +0100

    cosmetic

--- test/clientBrowserIEMobile.js
+++ test/clientBrowserIEMobile.js
@@ -4,27 +4,27 @@ const config = require('./lib/config');
 let defaultResult = config.defaultResult;
 
 let expected = {
-    "user_agent": {
-        "ua_string": "IEMobile 1.1",
-        "ua_class": "Mobile browser",
-        "ua_class_code": "mobile_browser",
-        "ua": "IE Mobile 1.1",
-        "ua_version": "1.1",
-        "ua_version_major": "1",
-        "ua_family": "IE Mobile",
-        "ua_family_code": "ie_mobile",
-        "ua_family_homepage": "https://en.wikipedia.org/wiki/Internet_Explorer_Mobile",
-        "ua_family_vendor": "Microsoft Corporation.",
-        "ua_family_vendor_code": "microsoft_corporation",
-        "ua_family_vendor_homepage": "https://www.microsoft.com/about/",
-        "ua_family_icon": "iemobile.png",
-        "ua_family_info_url": "https://udger.com/resources/ua-list/browser-detail?browser=IE Mobile",
-        "ua_engine": "Trident",
-        "device_class": "Smartphone",
-        "device_class_code": "smartphone",
-        "device_class_icon": "phone.png",
-        "device_class_icon_big": "phone_big.png",
-        "device_class_info_url": "https://udger.com/resources/ua-list/device-detail?device=Smartphone"
+    'user_agent': {
+        'ua_string': 'IEMobile 1.1',
+        'ua_class': 'Mobile browser',
+        'ua_class_code': 'mobile_browser',
+        'ua': 'IE Mobile 1.1',
+        'ua_version': '1.1',
+        'ua_version_major': '1',
+        'ua_family': 'IE Mobile',
+        'ua_family_code': 'ie_mobile',
+        'ua_family_homepage': 'https://en.wikipedia.org/wiki/Internet_Explorer_Mobile',
+        'ua_family_vendor': 'Microsoft Corporation.',
+        'ua_family_vendor_code': 'microsoft_corporation',
+        'ua_family_vendor_homepage': 'https://www.microsoft.com/about/',
+        'ua_family_icon': 'iemobile.png',
+        'ua_family_info_url': 'https://udger.com/resources/ua-list/browser-detail?browser=IE Mobile',
+        'ua_engine': 'Trident',
+        'device_class': 'Smartphone',
+        'device_class_code': 'smartphone',
+        'device_class_icon': 'phone.png',
+        'device_class_icon_big': 'phone_big.png',
+        'device_class_info_url': 'https://udger.com/resources/ua-list/device-detail?device=Smartphone'
     }
 };
 
