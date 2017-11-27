4b95e0adf52ce40870d3e6662fc04c39f9daba80
eviltik <eviltik@gmail.com>
2017-11-27 12:51:53 +0100

    add pingometer test

---
+++ test/clientCrawlerPingoMeter.js
@@ -0,0 +1,37 @@
+const tap = require('tap');
+const config = require('./lib/config');
+
+let defaultResult = config.defaultResult;
+
+let expected = {
+    'user_agent': {
+        'ua_string': 'PINGOMETER_BOT_(HTTPS://PINGOMETER.COM)',
+        'ua_class': 'Crawler',
+        'ua_class_code': 'crawler',
+        'ua': 'PINGOMETER',
+        'ua_family': 'PINGOMETER',
+        'ua_family_code': 'pingometer',
+        'ua_family_vendor': 'Pingometer, LLC',
+        'ua_family_vendor_code': 'pingometer_llc',
+        'ua_family_vendor_homepage': 'http://pingometer.com/',
+        'ua_family_icon': 'bot_pingometer.png',
+        'ua_family_info_url': 'https://udger.com/resources/ua-list/bot-detail?bot=PINGOMETER#id20112',
+        'crawler_last_seen': '2017-01-06 18:49:59',
+        'crawler_category': 'Site monitor',
+        'crawler_category_code': 'site_monitor',
+        'crawler_respect_robotstxt': 'no'
+    }
+};
+
+expected = config.merge(defaultResult, expected);
+
+tap.test(
+    'User Agent: PingoMeter should be recognized',
+    (t) => {
+        config.udgerParser.setUA('PINGOMETER_BOT_(HTTPS://PINGOMETER.COM)');
+        let ret = config.udgerParser.parse();
+        t.same(ret, expected);
+        t.end();
+    }
+);
+
