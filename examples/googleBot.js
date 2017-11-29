214461287f17de824464632bbec48e0556d47832
eviltik <eviltik@gmail.com>
2017-11-29 06:15:46 +0100

    add googlebot sample

---
+++ examples/googleBot.js
@@ -0,0 +1,11 @@
+const udgerParser = require('../')('test/db/udgerdb_v3_test.dat');
+
+udgerParser.set({
+    ua:'Googlebot/2.1 (+http://www.google.com/bot.html)',
+    ip:'66.249.64.73'
+});
+
+let ret = udgerParser.parse();
+
+// beautify json output with 4 spaces indent
+console.log(JSON.stringify(ret, null, 4));
